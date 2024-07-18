271dc368b27d3f10f73f25846e863cdac45f0f05
zsheng <admin@Admins-MBP.home>
2024-07-18 21:58:57 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Editor from './components/editor';
+
+function App() {
+  return (
+    <>
+      <div className='dark'>
+        <Editor />
+      </div>
+    </>
+  );
+}
+
+export default App;
