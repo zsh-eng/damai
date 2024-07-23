31b44a8275c199d8ef0f8dde873d336d7b1808f0
zsheng <admin@Admins-MBP.home>
2024-07-23 13:35:37 +0800

    feat: add command system

---
+++ src/commands/index.ts
@@ -0,0 +1,94 @@
+type DamaiCommand<_TPayload> = {
+  type: string;
+};
+
+type DamaiCommandPayload<TCommand extends DamaiCommand<unknown>> =
+  TCommand extends DamaiCommand<infer TPayload> ? TPayload : never;
+
+type DamaiCommandListener<TCommand extends DamaiCommand<unknown>> = (
+  ...args: DamaiCommandPayload<TCommand> extends never
+    ? []
+    : [payload: DamaiCommandPayload<TCommand>]
+) => void;
+
+// Recall that function signatures are covariant
+type DamaiCommandListenerMap = Map<
+  DamaiCommand<unknown>,
+  Set<DamaiCommandListener<DamaiCommand<never>>>
+>;
+
+const createDamaiCommand = <TPayload>(
+  type: string,
+): DamaiCommand<TPayload> => ({
+  type,
+});
+
+const VIEW_TOGGLE_PRIMARY_SIDEBAR_COMMAND = createDamaiCommand<never>(
+  "view:toggle-primary-sidebar",
+);
+
+const VIEW_TOGGLE_SECONDARY_SIDEBAR_COMMAND = createDamaiCommand<never>(
+  "view:toggle-secondary-sidebar",
+);
+
+const FILE_SELECT_COMMAND = createDamaiCommand<{ id: number }>("file:select");
+
+export const DAMAI_COMMANDS = {
+  VIEW_TOGGLE_PRIMARY_SIDEBAR_COMMAND,
+  VIEW_TOGGLE_SECONDARY_SIDEBAR_COMMAND,
+  FILE_SELECT_COMMAND,
+} as const;
+
+const listeners: DamaiCommandListenerMap = new Map();
+
+export const registerDamaiCommandListener = <
+  TCommand extends DamaiCommand<unknown>,
+>(
+  command: TCommand,
+  listener: DamaiCommandListener<TCommand>,
+) => {
+  if (!listeners.has(command)) {
+    console.debug(`No listeners for command ${command.type}. Creating set...`);
+    listeners.set(command, new Set());
+  }
+
+  const listenerSetForCommand = listeners.get(command);
+  if (!listenerSetForCommand) {
+    throw new Error("Listener set for command should exist");
+  }
+
+  listenerSetForCommand.add(listener);
+  console.debug(`Added listener for command ${command.type}`);
+
+  return () => {
+    console.debug(`Removing listener for command ${command.type}`);
+    listenerSetForCommand.delete(listener);
+  };
+};
+
+export const dispatchDamaiCommand = <TCommand extends DamaiCommand<unknown>>(
+  ...args: DamaiCommandPayload<TCommand> extends never
+    ? [command: TCommand]
+    : [command: TCommand, payload: DamaiCommandPayload<TCommand>]
+) => {
+  const [command, payload] = args;
+  console.debug(`Dispatching command ${command.type}`);
+  const listenerSetForCommand = listeners.get(command) as
+    | Set<DamaiCommandListener<TCommand>>
+    | undefined;
+
+  if (!listenerSetForCommand || listenerSetForCommand.size === 0) {
+    console.debug(`No listeners for command ${command.type}`);
+    return;
+  }
+
+  for (const listener of listenerSetForCommand) {
+    if (!payload) {
+      const typedListener = listener as DamaiCommandListener<never>;
+      typedListener();
+    } else {
+      // @ts-ignore - TS doesn't understand that payload is defined
+      listener(payload);
+    }
+  }
+};
