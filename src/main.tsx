1a5e54763052cf5858763e469bf895693a9626e7
zsheng <chengzhisheng2001@gmail.com>
2024-07-25 14:51:00 +0800

    style: change theme
    Update theme to use stone instead of slate.
    And changed the main font from Atkinson Hyperlegible to Inter variable.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import "@fontsource/atkinson-hyperlegible";
+import "@fontsource-variable/inter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
